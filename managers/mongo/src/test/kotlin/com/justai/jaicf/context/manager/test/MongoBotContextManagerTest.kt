df4e36af82ac659d484b3487b9312ac5e0dc39c0
veptechno <56798899+veptechno@users.noreply.github.com>
2021-08-09 15:53:21 +0300

    Update MongoBotContextManagerTest.kt

--- managers/mongo/src/test/kotlin/com/justai/jaicf/context/manager/test/MongoBotContextManagerTest.kt
+++ managers/mongo/src/test/kotlin/com/justai/jaicf/context/manager/test/MongoBotContextManagerTest.kt
@@ -21,58 +21,58 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MongoBotContextManagerTest {
 
-//     private lateinit var mongo: MongodExecutable
-//     private lateinit var manager: MongoBotContextManager
+    private lateinit var mongo: MongodExecutable
+    private lateinit var manager: MongoBotContextManager
 
     @BeforeAll
     internal fun setup() {
-//         val port = Network.getFreeServerPort()
-//         MongodConfig.builder()
-//             .version(Version.Main.PRODUCTION)
-//             .net(Net(port, false))
-//             .build().let { config ->
-//                 mongo = MongodStarter.getDefaultInstance().prepare(config)
-//                 mongo.start()
-//             }
+        val port = Network.getFreeServerPort()
+        MongodConfig.builder()
+            .version(Version.Main.PRODUCTION)
+            .net(Net(port, false))
+            .build().let { config ->
+                mongo = MongodStarter.getDefaultInstance().prepare(config)
+                mongo.start()
+            }
 
-//         val client = MongoClients.create("mongodb://localhost:$port")
-//         manager = MongoBotContextManager(client.getDatabase("jaicf").getCollection("contexts"))
+        val client = MongoClients.create("mongodb://localhost:$port")
+        manager = MongoBotContextManager(client.getDatabase("jaicf").getCollection("contexts"))
     }
 
     @AfterAll
     internal fun shutdown() {
-//         mongo.stop()
+        mongo.stop()
     }
 
     @Test
     fun testSave() {
-//         val context = BotContext("client1").apply {
-//             result = "some result"
-//             session["key1"] = "some value"
-//         }
+        val context = BotContext("client1").apply {
+            result = "some result"
+            session["key1"] = "some value"
+        }
 
-//         manager.saveContext(context, null, null, RequestContext.DEFAULT)
+        manager.saveContext(context, null, null, RequestContext.DEFAULT)
 
-//         val result = manager.loadContext(EventBotRequest("client1", "event"), RequestContext.DEFAULT)
+        val result = manager.loadContext(EventBotRequest("client1", "event"), RequestContext.DEFAULT)
 
-//         assertNotNull(result)
-//         assertEquals(context.result, result.result)
-//         assertEquals(context.session, result.session)
+        assertNotNull(result)
+        assertEquals(context.result, result.result)
+        assertEquals(context.session, result.session)
     }
 
     @Test
     fun testSaveCustomBean() {
-//         val context = BotContext("client2").apply {
-//             result = CustomValue(1)
-//             session["value"] = CustomValue(CustomValue(2))
-//         }
+        val context = BotContext("client2").apply {
+            result = CustomValue(1)
+            session["value"] = CustomValue(CustomValue(2))
+        }
 
-//         manager.saveContext(context, null, null, RequestContext.DEFAULT)
+        manager.saveContext(context, null, null, RequestContext.DEFAULT)
 
-//         val result = manager.loadContext(EventBotRequest("client2", "event"), RequestContext.DEFAULT)
+        val result = manager.loadContext(EventBotRequest("client2", "event"), RequestContext.DEFAULT)
 
-//         assertNotNull(result)
-//         assertTrue(result.result is CustomValue)
-//         assertTrue(result.session["value"] is CustomValue)
+        assertNotNull(result)
+        assertTrue(result.result is CustomValue)
+        assertTrue(result.session["value"] is CustomValue)
     }
 }
